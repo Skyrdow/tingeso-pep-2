31bdf3689fa2bc8bf27a766f7da0a2b3244a3970
Skyrdow <lucas.mesias@usach.cl>
2024-05-19 20:58:30 -0400

    fix: config properties

--- ms-reports/src/main/java/com/mingeso/msreports/MsReportApplication.java
+++ ms-reports/src/main/java/com/mingeso/msreports/MsReportApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class MsShippingApplication {
+public class MsReportApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsShippingApplication.class, args);
+		SpringApplication.run(MsReportApplication.class, args);
 	}
 
 }
